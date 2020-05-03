16fd912461c8c3424f4182bd2fd919f0f9987d95
Alec Wong <aw685@cornell.edu>
2020-05-03 14:57:08 -0400

    Change mind again; script for photo paths should reference server via ssh

--- scripts/make_photo_paths.sh
+++ scripts/make_photo_paths.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find static/photographs -type d | awk -f ./scripts/make_photo_paths.awk > static/photo_paths.txt
+ssh alecsalc@alecsalces.com 'pushd public_html > /dev/null; find static/photographs -type d' | awk -f ./scripts/make_photo_paths.awk > static/photo_paths.txt
