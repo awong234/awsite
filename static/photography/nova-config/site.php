5d1d99f4e1d6fc7c1d7228b22ddcce1602f18e12
Alec Wong <alecwong@live.com>
2024-02-02 10:08:49 -0500

    Add novagallery module

---
+++ static/photography/nova-config/site.php
@@ -0,0 +1,24 @@
+<?php defined("NOVA") or die(); ?>
+{
+  "siteName": "Gallery",
+  "siteTitle": "My Photographs",
+  "metaTitle": "novaGallery",
+  "metaDescription": "novaGallery - beautiful php photo gallery.",
+  "footerText": "&copy; 2021 by novafacile OÜ",
+  "theme": "novagallery",
+  "url": "https://alecsalces.com/photography",
+  "language": "en",
+  "allowedImageSizes": [
+      "400x400",
+      2000
+  ],
+  "imagesDirName": "galleries",
+  "imageCache": 1,
+  "imageSizeBig": 2000,
+  "imageSizeThumb": "400x400",
+  "imageQuality": 89,
+  "sortAlbums": "newest",
+  "sortImages": "newest",
+  "fileListMaxCacheAge": 60,
+  "pagePassword": ""
+}
