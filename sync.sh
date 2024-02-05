f3c59550c7d6a5d3ed096785706d7949b2d2f596
Alec Wong <alecwong@live.com>
2024-02-04 21:07:10 -0500

    Ignore cache directories, don't delete these

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # Synchronize with web server.
-rsync -avzP ./public/ alecsalc@alecsalces.com:/home2/alecsalc/public_html --delete --exclude=cgi-bin/
+rsync -avzP ./public/ alecsalc@alecsalces.com:/home2/alecsalc/public_html --delete --exclude=cgi-bin/ --exclude=cache/
