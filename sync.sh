c3842b5cda0b2aa96dfdccf15fa10c437ed64e2c
Alec Wong <alecwong@live.com>
2024-02-04 23:17:52 -0500

    Update sync, exclude galleries so they don't get deleted

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 # Synchronize with web server.
-rsync -avzP ./public/ alecsalc@alecsalces.com:/home2/alecsalc/public_html --delete --exclude=cgi-bin/ --exclude=cache/
+rsync -avzP ./public/ alecsalc@alecsalces.com:/home2/alecsalc/public_html --delete --exclude=cgi-bin/ --exclude=cache/ --exclude=galleries/
+
