12e66b159da2e3ce8384554fe55c527185c1f2e6
Alec Wong <alecwong@live.com>
2025-01-26 12:17:28 -0500

    Exclude photoshop files

--- gallery_sync.sh
+++ gallery_sync.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-rsync -avzP /mnt/c/Users/AlecW/OneDrive/Pictures/website-photos/ alecsalc@alecsalces.com:/home2/alecsalc/public_html/photography/galleries --delete --exclude=cache/
+rsync -avzP /mnt/c/Users/AlecW/OneDrive/Pictures/website-photos/ alecsalc@alecsalces.com:/home2/alecsalc/public_html/photography/galleries --delete --exclude=cache/ --exclude=*.psd
