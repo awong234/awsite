e76129096172c124f8fc8664e5fc743ff45c0190
Alec Wong <alecwong@live.com>
2026-07-18 23:03:11 -0400

    update sync script with inviztational

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 # Synchronize with web server.
 rsync -avzP ./public/ alecsalc@alecsalces.com:/home2/alecsalc/public_html --delete --exclude=cgi-bin/ --exclude=cache/ --exclude=galleries/
-
+# Send inviztational to website
+rsync -avzP /mnt/c/Users/AlecW/Documents/Local_Projects/inviztational2024/inviztational2024/ alecsalc@alecsalces.com:/home2/alecsalc/public_html/inviztational2024/
