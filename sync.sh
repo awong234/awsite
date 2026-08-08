f6fdb002a1bc6f8ebf50e59f45f692f75ec55162
Alec Wong <alecwong@live.com>
2026-08-07 21:54:00 -0400

    Change sync method

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
+# Synchronize inviztational here
+rsync -avzP /mnt/c/Users/AlecW/Documents/Local_Projects/inviztational2024/inviztational2024/ ./public/inviztational2024/
 # Synchronize with web server.
 rsync -avzP ./public/ alecsalc@alecsalces.com:/home2/alecsalc/public_html --delete --exclude=cgi-bin/ --exclude=cache/ --exclude=galleries/
-# Send inviztational to website
-rsync -avzP /mnt/c/Users/AlecW/Documents/Local_Projects/inviztational2024/inviztational2024/ alecsalc@alecsalces.com:/home2/alecsalc/public_html/inviztational2024/
